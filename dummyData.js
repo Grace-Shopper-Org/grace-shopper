cb12adb6d4f383bb5fbc555a38975efde01a504b
Simone Pincsak <simone.pincsak@gmail.com>
2020-08-04 15:20:04 -0500

    added fake data

---
+++ dummyData.js
@@ -0,0 +1,62 @@
+const fakeProducts = [
+  {
+    name: 'Soap A',
+    price: 5,
+    description: 'the first soap I could think of.',
+    imageUrl:
+      'https://images.unsplash.com/photo-1584305574620-2aab80eedfe6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
+  },
+  {
+    name: 'Lavender Dream',
+    price: 7,
+    description: 'smells like a lavender dream.',
+    imageUrl:
+      'https://images.unsplash.com/photo-1524136964799-6ba980a735f7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80'
+  },
+  {
+    name: 'Vanilla Vacation',
+    price: 6,
+    description: 'made from the finest vanilla beans',
+    imageUrl:
+      'https://images.unsplash.com/photo-1592788174877-3f99727fd23d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2800&q=80'
+  },
+  {
+    name: 'Cedar Surprise',
+    price: 6,
+    description: 'that good tree smell',
+    imageUrl:
+      'https://images.unsplash.com/photo-1560865302-90692d3a4716?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
+  },
+  {
+    name: 'Super Luxury Soap',
+    price: 10,
+    description: 'like a private jet but for cleaning',
+    imageUrl:
+      'https://images.unsplash.com/photo-1584305574647-0cc949a2bb9f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
+  },
+  {
+    name: 'Discount Value Soap',
+    price: 2,
+    description: 'Economy Value that gets the job done'
+  }
+]
+
+const fakeUsers = [
+  {
+    username: 'gottaHaveSoap',
+    password: '1234',
+    email: 'gottaHaveSoap@foo.bar'
+  },
+  {
+    username: 'cleanKid',
+    password: 'password',
+    email: 'cleanKid333@lysol.biz',
+    imageUrl:
+      'https://images.unsplash.com/photo-1504811190172-325dc0249d5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80'
+  },
+  {
+    username: 'graceHopper',
+    password: '5678',
+    email: 'grace@fullstack.org'
+  }
+]
