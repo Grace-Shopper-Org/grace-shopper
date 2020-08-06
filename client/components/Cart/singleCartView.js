4299856bb5af475c6a1e35c851e8ec0ca83a286d
Khuong <lekhuongy@gmail.com>
2020-08-06 15:23:05 -0500

    Skeleton of cart component

--- client/components/Cart/singleCartView.js
+++ client/components/Cart/singleCartView.js
@@ -1,6 +1,43 @@
-import React from 'react'
-import {connect} from 'react-redux'
+import React, {useState, useEffect} from 'react'
+import {useSelector, useDispatch} from 'react-redux'
+
+const dummyData = [
+  {
+    id: 1,
+    name: 'Soap A',
+    imgUrl: 'A.jpg',
+    price: '5',
+    quantity: '1'
+  },
+  {
+    id: 2,
+    name: 'Soap B',
+    imgUrl: 'B.jpg',
+    price: '2000',
+    quantity: '12'
+  }
+]
 
 function SingleCart(props) {
-  return <div>Hello World</div>
+  props = dummyData
+  const [cart, setCart] = useState([])
+
+  useEffect(() => {
+    setCart(dummyData)
+  })
+
+  return (
+    <div className="single-cart">
+      {cart.map(product => (
+        <div key={product.id} className="single-cart-item">
+          <div>{product.name}</div>
+          <div>{`$${product.price}`}</div>
+          <div>{product.quantity}</div>
+          <button type="button">Delete</button>
+        </div>
+      ))}
+    </div>
+  )
 }
+
+export default SingleCart
