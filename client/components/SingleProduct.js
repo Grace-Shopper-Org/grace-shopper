b1b09574b88a0f573ae5a5313b021b7d6e0930b6
Simone Pincsak <simone.pincsak@gmail.com>
2020-08-06 14:34:17 -0500

    single product view works

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -1,24 +1,18 @@
 import React from 'react'
-
-const testProduct = {
-  name: 'Cedar Surprise',
-  price: 6,
-  description: 'that good tree smell!',
-  imageUrl:
-    'https://images.unsplash.com/photo-1560865302-90692d3a4716?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
-}
+import {connect} from 'react-redux'
+import {fetchSingleProduct} from '../store/singleProduct'
 
 export class SingleProduct extends React.Component {
   componentDidMount() {
-    //later, this is where we'll call this.props.loadSingleProduct
-    console.log('the component has mounted!')
+    try {
+      this.props.loadSingleProduct(this.props.match.params.productId)
+    } catch (error) {
+      console.error(error)
+    }
   }
 
   render() {
-    //later:
-    //const currentProduct = this.props.singleProduct
-
-    const currentProduct = testProduct
+    const currentProduct = this.props.singleProduct
 
     return (
       <div>
@@ -37,9 +31,15 @@ export class SingleProduct extends React.Component {
 }
 
 //here's where the map State will go
+const mapState = reduxState => ({
+  singleProduct: reduxState.singleProduct
+})
 
 //and the mapDispatch
+const mapDispatch = dispatch => ({
+  loadSingleProduct: id => dispatch(fetchSingleProduct(id))
+})
 
 //and the export w/connect statement
 
-export default SingleProduct
+export default connect(mapState, mapDispatch)(SingleProduct)
